090b438489de8fd30574a10c1c7633520a99f004
Samuel Nicol <99494967+Github-Samuel@users.noreply.github.com>
2025-08-11 16:05:01 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author "Made with love by Samuel#0008"
+Version "1.0.0"
+
+shared_scripts {
+	'@ox_lib/init.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+files {
+    'html/*',
+}
+
+ui_page "html/index.html"
+
+lua54 'yes'
